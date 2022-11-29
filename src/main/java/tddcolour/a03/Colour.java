07d88ba07335c86ecad6885dd4d25ea24f1d959c
Rheena Blas <73996643+rheenablas@users.noreply.github.com>
2022-11-29 21:28:25 +0000

    One parameter Colour constructor created.

--- src/main/java/tddcolour/a03/Colour.java
+++ src/main/java/tddcolour/a03/Colour.java
@@ -1,5 +1,7 @@
 package tddcolour.a03;
 
+import java.util.Random;
+
 public class Colour {
 
     double red, green, blue;
@@ -7,5 +9,35 @@ public class Colour {
         this.red = red;
         this.green = green;
         this.blue = blue;
+    }
+
+    public Colour(int rgbTotal) {
+        int maxRed = 23;
+        int maxGreen = 15;
+        int maxBlue = 7;
+        int minRed = 16;
+        int minGreen = 8;
+        int minBlue = 0;
+        int leftOverColour = rgbTotal;
+
+        Random r = new Random();
+        red = r.nextInt(maxRed-minRed+1) + minRed;
+        leftOverColour -= red;
+        green = r.nextInt(maxGreen-minGreen+1) + minGreen;
+        leftOverColour -= green;
+        if (leftOverColour <= maxBlue && leftOverColour >= minBlue){
+            blue = leftOverColour;
+        }
+        else {
+            while (leftOverColour > maxBlue){
+                if (red < maxRed){
+                    red += 1;
+                    leftOverColour -= 1;
+                } else {
+                    green += 1;
+                    leftOverColour -= 1;
+                }
+            }
+        }
     }
 }
