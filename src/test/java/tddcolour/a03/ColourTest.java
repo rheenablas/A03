b93827fb12f8cfa4b3383918ce043169dd0d4edd
Rheena Blas <73996643+rheenablas@users.noreply.github.com>
2022-11-29 21:11:26 +0000

    Created first test function that checks if it has 3 parameters for constructor.

--- src/test/java/tddcolour/a03/ColourTest.java
+++ src/test/java/tddcolour/a03/ColourTest.java
@@ -1,7 +1,17 @@
 package tddcolour.a03;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColourTest {
 
+    Colour colour;
+    @Test
+    @DisplayName("Colour with three float parameters is not null")
+    void checkColourThreeParameters(){
+        colour = new Colour(0.0, 0.4, 0.65);
+        assertNotNull(colour);
+    }
 }
