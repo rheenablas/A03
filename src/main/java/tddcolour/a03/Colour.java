4d0031ad76d420179d59ad4f7395450a2a578f59
Rheena Blas <73996643+rheenablas@users.noreply.github.com>
2022-11-29 21:13:17 +0000

    Created first Colour constructor with 3 parameters.

--- src/main/java/tddcolour/a03/Colour.java
+++ src/main/java/tddcolour/a03/Colour.java
@@ -1,4 +1,11 @@
 package tddcolour.a03;
 
 public class Colour {
+
+    double red, green, blue;
+    public Colour(double red, double green, double blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
 }
